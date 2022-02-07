9acbbc38b031a5335fe8c25db6b3d36d33602f9b
Ivo Petrov <1414vo@gmail.com>
2022-02-07 22:55:26 +0000

    Added Category component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Category_Box from './components/Category_Box';
 function App() {
   return (
     <div className="App">
@@ -18,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <Category_Box/>
       </header>
     </div>
   );
---
+++ src/components/Category_Box.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Category_Box.css';
+
+class Category_Box extends React.Component {
+    render() {
+        return (
+            <div className="category_box">
+              This is a box!
+            </div>
+          );
+    }
+  
+}
+
+export default Category_Box;
