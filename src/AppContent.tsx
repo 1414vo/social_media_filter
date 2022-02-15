7a997e23a0090332ecbae1b1ac10ec2a292dc182
Ivo Petrov <1414vo@gmail.com>
2022-02-15 14:38:29 +0000

    Dynamic changes for categories split

--- src/AppContent.tsx
+++ src/AppContent.tsx
@@ -18,64 +18,67 @@ interface IAppState {
 }
 
 class AppContent extends React.Component<IAppProps, IAppState> {
-  constructor(props: IAppProps) {
-    super(props);
-    this.state = {
-      toxic: [],
-      medium: [],
-      light: []
+    constructor(props: IAppProps) {
+        super(props);
+        this.state = AppContent.splitIntoColumns(this.props);
     }
-    this.splitIntoColumns();
-  }
 
-  toggleCategory(category: CategoryType) {
-    console.log(CategoryType[category]);
-  }
-
-  splitIntoColumns() {
-    this.props.categories.forEach(cat => {
-      var v = Category.getCategoryScore(cat);
-      switch(v) {
-        case 0:
-          this.state.toxic.push(cat);
-          break;
-        case 1:
-          this.state.medium.push(cat);
-          break;
-        case 2:
-          this.state.light.push(cat);
-          break;
-      }
-    })
-  }
-
-  
-
-  render() {
-    if(this.props.tabIndex == 0){
-        return (
-            <div className="row">
-                <div className="column-1">
-                {
-                    this.state.toxic.map(cat => <CategoryBox category={cat} onClick={() => this.toggleCategory(cat)}/>)
-                }
-                </div>
-                <div className="column-2">
-                {
-                    this.state.medium.map(cat => <CategoryBox category={cat} onClick={() => this.toggleCategory(cat)}/>)
-                }
-                </div>
-                <div className="column-3">
-                {
-                    this.state.light.map(cat => <CategoryBox category={cat} onClick={() => this.toggleCategory(cat)}/>)
-                }
-                </div>
-            </div>
-        );
-    }else{
-        return (<div></div>);
+    toggleCategory(category: CategoryType) {
+        console.log(CategoryType[category]);
     }
-  }
+
+    static splitIntoColumns(props: IAppProps) {
+        var splitProps: IAppState = {
+            toxic: [],
+            medium: [],
+            light: []
+        }
+        props.categories.forEach(cat => {
+            var v = Category.getCategoryScore(cat);
+            switch(v) {
+            case 0:
+                splitProps.toxic.push(cat);
+                break;
+            case 1:
+                splitProps.medium.push(cat);
+                break;
+            case 2:
+                splitProps.light.push(cat);
+                break;
+            }
+        });
+        return splitProps;
+    }
+
+    static getDerivedStateFromProps(props: IAppProps, state: IAppState) {
+        return AppContent.splitIntoColumns(props);
+    }
+
+    render() {
+        if(this.props.tabIndex == 0){
+            return (
+                <div className="row">
+                    <div className="column-1">
+                    {
+                        this.state.toxic.map(cat => <CategoryBox category={cat} onClick={() => this.toggleCategory(cat)}/>)
+                    }
+                    </div>
+                    <div className="column-2">
+                    {
+                        this.state.medium.map(cat => <CategoryBox category={cat} onClick={() => this.toggleCategory(cat)}/>)
+                    }
+                    </div>
+                    <div className="column-3">
+                    {
+                        this.state.light.map(cat => <CategoryBox category={cat} onClick={() => this.toggleCategory(cat)}/>)
+                    }
+                    </div>
+                </div>
+            );
+        }else{
+            return (<div></div>);
+        }
+    }
 }
 
 export default AppContent;
