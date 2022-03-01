127f4fe2bb9e02e8317c4bb17d5784794eaa25db
Matthew Elliot <52085384+Matthew-07@users.noreply.github.com>
2022-03-01 14:16:15 +0000

    init

---
+++ public/parse.js
@@ -0,0 +1,114 @@
+var active_categories; // Set of categories currently active
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension");
+    // Recieved a message, for now assume this message tells us to change categories
+    active_categories = request.categories;
+    console.log(active_categories);
+  }
+);
+
+var parsedTweets = new Set();
+
+var tweetScores = new Map();
+
+function getTweetText(tweet){
+    var spans = tweet.getElementsByTagName("span");
+
+    var text = "";
+
+    for (var j = 3; j < spans.length - 9; ++j){
+        text = text + spans[j].innerText + " ";
+    }
+
+    return text;
+}
+
+function parseTweets(tweets){
+  var newTweets = new Set();
+  for (const tweet of tweets){
+    newTweets.add(tweet);
+  }
+
+  if (parsedTweets == null){
+    parsedTweets = newTweets;
+  } else {
+    parsedTweets.forEach(function(tweet){ 
+      newTweets.delete(tweet); 
+    });
+
+    newTweets.forEach(function(tweet){
+      parsedTweets.add(tweet); 
+    });
+  }
+
+  for (tweet of newTweets){
+    var text = getTweetText(tweet);
+
+    if (!tweetScores.has(text)){
+        tweetScores.set(text, null);
+        
+        // TODO: Send http request to classify text
+        requestScore(text);
+
+        // TEMP: randomly decide whether to display tweets
+        tweetScores.set(text, Math.random() > 0.5);
+    }
+
+    //document.body.insertAdjacentHTML("beforebegin", "<p>" + text + "</p>");
+  }
+
+  return newTweets;
+}
+
+function displayTweets(tweets){
+    for (tweet of tweets){
+        var score = tweetScores.get(getTweetText(tweet));
+
+        // Determine whether or not to display tweet
+
+        // TEMP
+        if (!score){
+            //tweet.innerText = "HIDDEN";
+            tweet.style.display = "None";            
+        }
+        
+        // For now, change text
+    }
+}
+
+function requestScore(text){
+    const xhr = new XMLHttpRequest();
+
+    xhr.open("POST", "http://127.0.0.1:5000/tweet?value=" + text);
+
+    xhr.send();
+
+    xhr.onload = function(){
+        if (xhr.status == 200){
+
+        } else if(xhr.status == 404){
+            console.log("Error: No classification recieved!");
+
+            var data = JSON.parse(xhr.responseText);
+
+            console.log(data);
+        }
+    }
+
+    //xhr.onerror = function(){...}
+
+    //xhr.onprogress = function(e){...} - probably don't need to react to progress, responces should be small
+}
+
+function run(){
+    var tweets = document.querySelectorAll('[data-testid="tweet"]');
+    parseTweets(tweets);
+    displayTweets(tweets);
+}
+
+setInterval(run, 10);
+//window.onchange = run;
--- public/start.js
+++ public/start.js
@@ -1,3 +1,19 @@
+var categories = new Set([1,2,3]);
+
+function set_to_json(set){
+    json_string = "[";
+
+    var addComma = false;
+
+    for (let element of set){
+        if (addComma) json_string += ",";
+        json_string += "\"" + element + "\"";
+        addComma = true;
+    }
+
+    return json_string + "]";
+}
+
 chrome.runtime.onInstalled.addListener((details) =>
 {
     console.log("Installed");
@@ -5,3 +21,9 @@ chrome.runtime.onInstalled.addListener((details) =>
 self.addEventListener('message', function (msg) {
     console.log(msg.data);
 });
+
+setInterval(() => {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {categories: set_to_json(categories)});
+    }); 
+}, 1000);
