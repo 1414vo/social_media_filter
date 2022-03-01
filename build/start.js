b72d76c3c42de2f1dc12597599edf0782973fd9d
Ivo Petrov <1414vo@gmail.com>
2022-03-01 21:56:00 +0000

    Have changes persist after closing popup

--- build/start.js
+++ build/start.js
@@ -1,7 +1,48 @@
+var categories = new Set([1,2,3]);
+
+function set_to_json(set){
+    json_string = "[";
+
+    var addComma = false;
+
+    for (let element of set){
+        if (addComma) json_string += ",";
+        json_string += "\"" + element + "\"";
+        addComma = true;
+    }
+
+    return json_string + "]";
+}
+
 chrome.runtime.onInstalled.addListener((details) =>
 {
     console.log("Installed");
 });
+
+categoryMap = [];
+primaryList = [];
+secondaryList = [];
+avoidList = [];
+
 self.addEventListener('message', function (msg) {
-    console.log(msg.data);
+    if(msg.data['changeCategory']){
+        console.log(msg, msg.data.changeCategory);
+        categoryMap = Array.from(msg.data.changeCategory);
+        this.chrome.storage.sync.set({'categoryMap': categoryMap});
+        console.log(categoryMap);
+    }
+    if(msg.data['changeLists']) {
+        console.log(msg);
+        primaryList = msg.data['changeLists'].primaryList;
+        secondaryList = msg.data['changeLists'].secondaryList;
+        avoidList = msg.data['changeLists'].avoidList;
+        this.chrome.storage.sync.set({'primaryList': primaryList, 'secondaryList': secondaryList, 'avoidList': avoidList});
+        console.log(categoryMap);
+    }
 });
+
+setInterval(() => {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {categories: set_to_json(categories)});
+    }); 
+}, 1000);
--- public/start.js
+++ public/start.js
@@ -2,6 +2,31 @@ chrome.runtime.onInstalled.addListener((details) =>
 {
     console.log("Installed");
 });
+
+categoryMap = [];
+primaryList = [];
+secondaryList = [];
+avoidList = [];
+
 self.addEventListener('message', function (msg) {
-    console.log(msg.data);
+    if(msg.data['changeCategory']){
+        console.log(msg, msg.data.changeCategory);
+        categoryMap = Array.from(msg.data.changeCategory);
+        this.chrome.storage.sync.set({'categoryMap': categoryMap});
+        console.log(categoryMap);
+    }
+    if(msg.data['changeLists']) {
+        console.log(msg);
+        primaryList = msg.data['changeLists'].primaryList;
+        secondaryList = msg.data['changeLists'].secondaryList;
+        avoidList = msg.data['changeLists'].avoidList;
+        this.chrome.storage.sync.set({'primaryList': primaryList, 'secondaryList': secondaryList, 'avoidList': avoidList});
+        console.log(categoryMap);
+    }
 });
+
+setInterval(() => {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {categories: set_to_json(categories)});
+    }); 
+}, 1000);
