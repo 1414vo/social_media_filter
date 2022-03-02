721f740f4c4fe8281af7de2d981fbe8d14f67dc2
kmhangus <kmhangus123@gmail.com>
2022-03-02 10:07:16 +0000

    Revert changes in previous 3 commits

--- build/start.js
+++ build/start.js
@@ -5,19 +5,3 @@ chrome.runtime.onInstalled.addListener((details) =>
 self.addEventListener('message', function (msg) {
     console.log(msg.data);
 });
-
-var color = '#ffffff';
-
-export async function updateBackgroundColor(newColor) {
-    color = newColor;
-    
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true }); 
-    chrome.scripting.executeScript({ 
-        target: { tabId: tab.id }, 
-        function: setBackgroundColor
-    });
-}
-
-function setBackgroundColor() {
-    document.body.style.backgroundColor = color;
-}
