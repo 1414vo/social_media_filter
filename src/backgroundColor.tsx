4bda306835e97178db680a2f10a6f80e9a735663
kmhangus <kmhangus123@gmail.com>
2022-03-02 14:27:47 +0000

    Colouring (3.4.5)

--- src/backgroundColor.tsx
+++ src/backgroundColor.tsx
@@ -2,7 +2,9 @@ export async function changeBackgroundColor() {
     await chrome.tabs.query({active: true, currentWindow: true}, 
     (
       r => {
-        chrome.scripting.executeScript({target: {tabId: r[0].id!}, file: './changeBackgroundColor.js'})
+        chrome.scripting.executeScript({
+            target: {tabId: r[0].id!}, 
+            files: ['./changeBackgroundColor.js']})
       }
     ));
 }
