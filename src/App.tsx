e21f3d0c942ab378afc470200f045745f74f44d8
kmhangus <kmhangus123@gmail.com>
2022-03-02 12:41:57 +0000

    Revert changes made to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -27,28 +27,12 @@ class App extends React.Component<IAppProps, IAppState> {
     });
   }
 
-  async changeBackgroundColor() {
-    this.updateTabIndex(1); 
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true }); 
-
-    chrome.scripting.executeScript({ 
-      target: { tabId: tab.id }, 
-      function: this.setBackgroundColor, 
-    });
-  }
-
-  setBackgroundColor() {
-    chrome.storage.sync.get(['color'], (data) => {
-      document.body.style.backgroundColor = data.color; 
-    });
-  }
-
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <div onClick={() => this.updateTabIndex(0)} className="Header-tab"><Tab title="Home" isSelected={this.state.tabIndex == 0}></Tab></div>
-          <div onClick={() => this.changeBackgroundColor()} className="Header-tab"><Tab title="Your Feed" isSelected={this.state.tabIndex == 1}></Tab></div>
+          <div onClick={() => this.updateTabIndex(1)} className="Header-tab"><Tab title="Your Feed" isSelected={this.state.tabIndex == 1}></Tab></div>
         </div>
         <div className="App-content">
             <AppContent tabIndex={this.state.tabIndex}></AppContent>
