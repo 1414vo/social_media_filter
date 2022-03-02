842ce223d4264a9a630b60d39f893d51028a6910
kmhangus <kmhangus123@gmail.com>
2022-03-02 13:58:03 +0000

    Colouring (3.4.2)

--- src/backgroundColor.tsx
+++ src/backgroundColor.tsx
@@ -2,13 +2,11 @@ export async function changeBackgroundColor() {
     await chrome.tabs.query({active: true, currentWindow: true}, 
     (
       r => {
-        if (!!r) {
-            chrome.tabs.executeScript(r[0].id , {file: './changeBackgroundColor.js'}, function() {
-                if(chrome.runtime.lastError) {
-                  console.error("Script injection failed: " + chrome.runtime.lastError.message);
-                }
-            })
-        }
+        chrome.tabs.executeScript(r[0].id!, {file: './changeBackgroundColor.js'}, function() {
+          if(chrome.runtime.lastError) {
+            console.error("Script injection failed: " + chrome.runtime.lastError.message);
+          }
+        })
       }
     ));
 }
