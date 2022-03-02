6cab75242c4e32242a2f1b17d6d87fdadc1b351c
Matthew Elliot <52085384+Matthew-07@users.noreply.github.com>
2022-03-02 15:03:42 +0000

    HTTP requests

--- build/start.js
+++ build/start.js
@@ -1,7 +1,33 @@
+var active_categories = new Set();
+
+function set_to_json(set){
+    json_string = "[";
+
+    var addComma = false;
+
+    for (let element of set){
+        if (addComma) json_string += ",";
+        json_string += element;
+        addComma = true;
+    }
+
+    return json_string + "]";
+}
+
 chrome.runtime.onInstalled.addListener((details) =>
 {
     console.log("Installed");
 });
 self.addEventListener('message', function (msg) {
+    console.log("ServiceWorkerMsg:")
     console.log(msg.data);
+    if (msg.data.isOn){
+        active_categories.add(msg.data.category)
+    } else {
+        active_categories.delete(msg.data.category);
+    }
+    console.log(active_categories);
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, set_to_json(active_categories));
+    });
 });
--- src/backend/app.py
+++ src/backend/app.py
@@ -1,6 +1,8 @@
 import time
+from urllib import response
 from flask import Flask
 from flask import request
+from flask import Response
 from process import Scorer
 
 app = Flask(__name__)
@@ -8,11 +10,19 @@ scorer = Scorer()
 
 @app.route('/time')
 def get_current_time():
-    return {'time': time.time()}
+    #resp = flask.Response('{"time": {"Politics": 0.2525, "Art": 0.6246}}')
+    resp = Response('{"time": {"Politics": 0.2525, "Art": 0.6246}}')
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
+    #return {'time': time.time()}
 
-@app.route('/score', methods=['GET'])
+@app.route('/score', methods=['POST'])
 def get_sentence_score():
     error = None
-    if request.method == 'GET':
-        print(request.form['text'])
-        return scorer.score(request.form['text'])
+    if request.method == 'POST':
+        resp_dict = {"category_scores": scorer.score(request.form['text'])}
+        resp_text = str(resp_dict)
+        resp_text = resp_text.replace("'", '"') # JSON doesn't seem to like single quotes.
+        resp = Response(resp_text)
+        resp.headers['Access-Control-Allow-Origin'] = '*'
+        return resp
