2e9ccf83eefcd7ecbbac7659b6944621affaeb22
kmhangus <kmhangus123@gmail.com>
2022-03-03 11:48:22 +0000

    Colouring (4)

--- build/start.js
+++ build/start.js
@@ -1,7 +1,12 @@
+import {changeColor} from "./changeBackgroundColor.js";
+
 chrome.runtime.onInstalled.addListener((details) =>
 {
     console.log("Installed");
 });
 self.addEventListener('message', function (msg) {
     console.log(msg.data);
+    if (msg.data['color']) {
+        changeColor(msg.color);
+    }
 });
