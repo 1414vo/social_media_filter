5fd53fd79753ba23d26dd0da7b2cd42aacf0c8ae
Matthew Elliot <52085384+Matthew-07@users.noreply.github.com>
2022-03-08 16:45:41 +0000

    start.js documentation
    Added comments and removed redundant code from start.js.

--- build/start.js
+++ build/start.js
@@ -1,24 +1,10 @@
-var active_categories = new Set();
-
-function set_to_json(set){
-    json_string = "[";
-
-    var addComma = false;
-
-    for (let element of set){
-        if (addComma) json_string += ",";
-        json_string += element;
-        addComma = true;
-    }
-
-    return json_string + "]";
-}
-
+// Console message when extension installed
 chrome.runtime.onInstalled.addListener((details) =>
 {
     console.log("Installed");
 });
 
+active_categories = new Set();
 categoryMap = [];
 primaryList = [];
 secondaryList = [];
