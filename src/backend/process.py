a7ab55ac5ef4bd2bd103346252ccd584a722da45
fuiszgt <fuiszgabor@gmail.com>
2022-03-10 01:23:43 +0000

    Refactor code and add comments

--- src/backend/process.py
+++ src/backend/process.py
@@ -4,11 +4,6 @@ import json
 
 SAMPLES_FILE = "samples.json"
 
-politics = "San Francisco voters oust three school board members in recall vote, CNN projects"
-topics = ["Politics", "Entertainment", "Art", "Music", "Lifestyle", "Academic", "Comedy", "Inspirational", "News",
-          "Business", "Tech", "Sports"]
-prompts = {topic: f"A tweet about {topic}" for topic in topics}
-
 
 def read_samples():
     # Open file specified in SAMPLES_FILE
@@ -18,15 +13,20 @@ def read_samples():
 
 
 class Scorer:
+
+    # Create reference embeddings for each category
     def __init__(self):
         tweets_by_topic = read_samples()
         self.model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
-        self.prompt_vectors = {
+
+        # Reference embeddings are the means of embeddings of sample tweets in each category
+        self.reference_embeddings = {
             topic: np.mean([np.array(self.model.encode(tweet)) for tweet in tweets_of_topic], axis=0)
             for (topic, tweets_of_topic)
             in tweets_by_topic.items()
         }
 
+    # Compare a new tweet against each reference embedding to calculate similarity scores by category
     def score(self, sentence):
         embedding = np.array(self.model.encode(sentence))
-        return {topic: prompt_vector.dot(embedding).item() for (topic, prompt_vector) in self.prompt_vectors.items()}
+        return {topic: prompt_vector.dot(embedding).item() for (topic, prompt_vector) in self.reference_embeddings.items()}
