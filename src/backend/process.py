88856a9c8a3716cfe08947fdf9e0416b9ff7d5e0
fuiszgt <fuiszgabor@gmail.com>
2022-03-10 00:50:51 +0000

    Read from file and create sample file

--- src/backend/process.py
+++ src/backend/process.py
@@ -1,12 +1,24 @@
 from sentence_transformers import SentenceTransformer
 import numpy as np
+import json
+
+SAMPLES_FILE = "samples.json"
 
 politics = "San Francisco voters oust three school board members in recall vote, CNN projects"
-topics = ["Politics", "Entertainment", "Art", "Music", "Lifestyle", "Academic", "Comedy", "Inspirational", "News", "Business", "Tech", "Sports"]
+topics = ["Politics", "Entertainment", "Art", "Music", "Lifestyle", "Academic", "Comedy", "Inspirational", "News",
+          "Business", "Tech", "Sports"]
 prompts = {topic: f"A tweet about {topic}" for topic in topics}
 
 
+def read_samples():
+    # Open file specified in SAMPLES_FILE
+    with open(SAMPLES_FILE, "r") as samples_file:
+        samples = json.load(samples_file)
+    return samples
+
+
 class Scorer:
+
     def __init__(self):
         self.model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
         self.prompt_vectors = {topic: np.array(self.model.encode(prompt)) for (topic, prompt) in prompts.items()}
